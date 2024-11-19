6f81651d6b21010680bfc9fa5ec7301c321b0919
hsiwei.kao <hsiwei.kao@hdrenewables.com>
2024-11-19 19:02:13 +0800

    321

--- src/main/java/com/example/email1/SpringEmailDemo1Application.java
+++ src/main/java/com/example/email1/SpringEmailDemo1Application.java
@@ -8,6 +8,7 @@ public class SpringEmailDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmailDemo1Application.class, args);
+		System.out.print("你好")    ;     
 	}
 
 }
